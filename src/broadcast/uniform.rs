c66c44c1de129358ddd19899d3edcdaafd942012
Ogier Bouvier <ars3nic@ars3nic.ch>
2020-07-20 16:12:08 +0200

    broadcast/uniform: fix issues and regression test
    Signed-off-by: Ogier Bouvier <ars3nic@ars3nic.ch>

--- src/broadcast/uniform.rs
+++ src/broadcast/uniform.rs
@@ -1,3 +1,6 @@
+use std::collections::hash_map::Entry;
+use std::collections::{HashMap, HashSet};
+
 use super::{BestEffort, BroadcastError, Broadcaster, Deliverer};
 use crate::{Message, System};
 
@@ -8,13 +11,11 @@ use drop::net::SendError;
 use futures::future::Either;
 use futures::FutureExt;
 
-use hashbrown::hash_map::Entry;
-use hashbrown::{HashMap, HashSet};
-
 use tokio::sync::mpsc;
 use tokio::task::{self, JoinHandle};
 
-use tracing::warn;
+use tracing::{debug_span, info, warn};
+use tracing_futures::Instrument;
 
 /// A `Broadcast` implementation that provides the following guarantees:
 /// * all correct process will eventually deliver a `Message` broadcasted by a
@@ -41,6 +42,7 @@ impl UniformReliable {
     }
 }
 
+/// The sending end of the `UniformReliable` broadcast primitive'
 pub struct UniformReliableBroadcaster<M: Message + 'static> {
     msg_tx: mpsc::Sender<M>,
     error_rx: mpsc::Receiver<BroadcastError>,
@@ -66,6 +68,10 @@ impl<M: Message + 'static> Broadcaster<M> for UniformReliableBroadcaster<M> {
         } else {
             self.error_rx.recv().await.unwrap_or(None)
         }
+    }
+
+    fn known_peers(&self) -> HashSet<PublicKey> {
+        todo!()
     }
 }
 
@@ -130,12 +136,17 @@ where
         match self.pending.entry(message.clone()) {
             Entry::Occupied(mut e) => {
                 if e.get_mut().insert(*pkey) {
-                    todo!("check if all acks are  here")
+                    info!("new ack from {} for {:?}", pkey, message);
+                    let peers = self.outgoing.known_peers();
+
+                    peers.difference(e.get()).count() == 0
                 } else {
                     false
                 }
             }
             Entry::Vacant(e) => {
+                info!("unseen message {:?}", message);
+
                 let mut new_acks = HashSet::default();
                 new_acks.insert(*pkey);
                 e.insert(new_acks);
@@ -153,36 +164,47 @@ where
     }
 
     fn task(mut self) -> JoinHandle<(B, D)> {
-        task::spawn(async move {
-            loop {
-                match self.poll().await {
-                    Either::Right(Some(ref msg)) => {
-                        let result = self.outgoing.broadcast(msg).await;
-                        if self.error_tx.send(result).await.is_err() {
-                            warn!("broadcaster has been dropped");
+        task::spawn(
+            async move {
+                let mut peer_warn = true;
+
+                loop {
+                    match self.poll().await {
+                        Either::Right(Some(ref msg)) => {
+                            let result = self.outgoing.broadcast(msg).await;
+                            if self.error_tx.send(result).await.is_err() {
+                                warn!("broadcaster has been dropped");
+                            }
                         }
-                    }
-                    Either::Right(None) => {
-                        warn!("no more peers available in system");
-                        break;
-                    }
-                    Either::Left(Some((pkey, msg))) => {
-                        if self.can_deliver(&pkey, &msg)
-                            && self.msg_tx.send((pkey, msg)).await.is_err()
-                        {
-                            warn!("no more incoming messages");
+                        Either::Right(None) => {
+                            if peer_warn {
+                                warn!("no more peers available in system");
+                                peer_warn = false;
+                            }
+
+                            continue;
+                        }
+                        Either::Left(Some((pkey, msg))) => {
+                            if self.can_deliver(&pkey, &msg) {
+                                info!("all acks for {:?} delivering", msg);
+                                if self.msg_tx.send((pkey, msg)).await.is_err()
+                                {
+                                    warn!("no more incoming messages");
+                                    break;
+                                }
+                            }
+                        }
+                        Either::Left(None) => {
+                            warn!("end of broadcast stream");
                             break;
                         }
                     }
-                    Either::Left(None) => {
-                        warn!("end of broadcast stream");
-                        break;
-                    }
                 }
-            }
 
-            (self.outgoing, self.incoming)
-        })
+                (self.outgoing, self.incoming)
+            }
+            .instrument(debug_span!("ack_task")),
+        )
     }
 }
 
@@ -232,5 +254,63 @@ mod test {
         assert_eq!(value, VALUE, "wrong value delivered");
 
         handle.await.expect("panic");
+    }
+
+    #[tokio::test]
+    async fn broadcast_then_deliver() {
+        const VALUE: usize = 200;
+
+        init_logger();
+
+        let (_, handle, system) =
+            create_system(50, |mut connection| async move {
+                let value: usize =
+                    connection.receive().await.expect("failed to receive");
+                assert_eq!(value, VALUE, "wrong value received");
+                connection.send(&value).await.expect("failed to send");
+            })
+            .await;
+
+        let (mut bcast, mut delivery) = UniformReliable::with::<usize>(system);
+
+        bcast.broadcast(&VALUE).await.expect("failed to broadcast");
+
+        let (_, delivered) = delivery.deliver().await.expect("deliver failed");
+
+        assert_eq!(delivered, VALUE, "wrong value delivered");
+
+        handle.await.expect("system failure");
+    }
+
+    #[tokio::test]
+    async fn failed_peer_does_not_deliver() {
+        use std::sync::atomic::{AtomicBool, Ordering};
+
+        const VALUE: &str = "Test message no1";
+        static SHOULD_FAIL: AtomicBool = AtomicBool::new(true);
+
+        init_logger();
+
+        let (_, handle, system) =
+            create_system(50, |mut connection| async move {
+                if !SHOULD_FAIL.compare_and_swap(true, false, Ordering::AcqRel)
+                {
+                    connection
+                        .send(&VALUE.to_string())
+                        .await
+                        .expect("send failed");
+                }
+
+                connection.close().await.expect("close failed");
+            })
+            .await;
+
+        let (_, mut delivery) = UniformReliable::with::<String>(system);
+
+        let message = delivery.deliver().await;
+
+        assert!(message.is_none(), "delivered unacked message");
+
+        handle.await.expect("system failure");
     }
 }
